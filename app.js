c3a4bec65902cb53054a44fd5dbf93a752586733
Harrison Muskat <harrisonmuskat@gmail.com>
2017-11-08 10:16:56 -0500

    Lodash stuff

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(3,4));
+// console.log(_.isString(true));
+// console.log(_.isString('Harrison'));
+var filteredArray = _.uniq(['Harrison', 1, 'Harrison', 1, 2, 3, 4]);
+console.log(filteredArray);
+//console.log('Result:', notes.add(3,4));
 
 // var user = os.userInfo();
 
