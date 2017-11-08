0adc27f38c34df921643448a3d363f1416f3cd21
Harrison Muskat <harrisonmuskat@gmail.com>
2017-11-08 10:33:28 -0500

    Prepping for actual notes app

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Harrison'));
-var filteredArray = _.uniq(['Harrison', 1, 'Harrison', 1, 2, 3, 4]);
-console.log(filteredArray);
-//console.log('Result:', notes.add(3,4));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
